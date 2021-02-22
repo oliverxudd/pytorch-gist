37116a4bfb1ecf95db65a3dfafea9ee5110cdaa3
oliverxudd <55381843+oliverxudd@users.noreply.github.com>
2021-02-22 21:21:55 +0800

    Create python_env_setup.py

---
+++ python_env_setup.py
@@ -0,0 +1,6 @@
+"""environment setup for python"""
+
+# 1. add module-search path
+import sys
+# sys.path contains a list of paths that the Python interpreter will search for a module.
+sys.path.append('/your/path/')
