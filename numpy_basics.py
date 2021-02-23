0df52128539cce1c5266e32ec03590252282167c
oliverxudd <55381843+oliverxudd@users.noreply.github.com>
2021-02-23 20:50:34 +0800

    Update numpy_basics.py

--- numpy_basics.py
+++ numpy_basics.py
@@ -7,3 +7,10 @@ B[:] = A
 # 2. rearrange axes for numpy array
 np.transpose(A, axes=[2,0,1])
 A.transpose(2,0,1)
+
+# 3. set elements to 0 if element meet some condition
+out = np.where(A<3, A, 0) # 将矩阵A中小于3的元素置为0
+
+# 4. show nonzero elements of numpy array
+A = np.random.randint(0, 100, (10, 10))
+(coords0, coords1) = numpy.nonzero(A)
