580194a8e2d9f66946346ea3b5e960030c91d38d
oliverxudd <55381843+oliverxudd@users.noreply.github.com>
2021-02-23 16:13:14 +0800

    Update numpy_basics.py

--- numpy_basics.py
+++ numpy_basics.py
@@ -1,5 +1,9 @@
 """basics of numpy"""
 
-1. deep copy of numpy arrays
+# 1. deep copy of numpy arrays
 B = numpy.empty_like(A)
 B[:] = A
+
+# 2. rearrange axes for numpy array
+np.transpose(A, axes=[2,0,1])
+A.transpose(2,0,1)
