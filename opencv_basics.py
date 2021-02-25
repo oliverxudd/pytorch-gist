9926138a4a8a07679a781a2f9b5864e39b9acb65
oliverxudd <55381843+oliverxudd@users.noreply.github.com>
2021-02-25 11:14:44 +0800

    Update opencv_basics.py

--- opencv_basics.py
+++ opencv_basics.py
@@ -3,3 +3,12 @@
 # 1. add borders to image
 Bsize = 2 
 image = cv2.copyMakeBorder(image, top=Bsize, left=Bsize, right=Bsize, bottom=Bsize, borderType=cv2.BORDER_CONSTANT, value=0)
+
+# 2. normalize the norm or value range of an array
+# 2.1 normalize the norm
+A = cv2.normalize(A, dst=None, alpha=1, beta=0, norm_type=cv2.NORM_L2)
+
+# 2.2 normalize the value range
+A = np.array([10, 11, 15, 20])
+A = cv2.normalize(A, dst=None, alpha=0, beta=10, norm_type=cv2.NORM_MINMAX)
+#                              min      max                    scale & shift the array.
